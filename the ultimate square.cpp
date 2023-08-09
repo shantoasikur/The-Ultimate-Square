24324c27db334134f6cf1a593d481bfb9766c7ed
shantoasikur <141675776+shantoasikur@users.noreply.github.com>
2023-08-10 03:12:00 +0600

    Add files via upload

---
+++ the ultimate square.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+int main(){
+int  t;
+cin>>t;
+while(t--){
+    int n;
+    long long result;
+    cin>>n;
+    result=ceil((double)n/2);
+    cout<<result<<endl;
+}
+
+
+
+
+
+}
